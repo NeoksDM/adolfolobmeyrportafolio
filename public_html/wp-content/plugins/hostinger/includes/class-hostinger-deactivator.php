9f8587e7b1f4998f6bad915825c2b59507f70b57
Neoks <neokspp@proton.me>
2024-04-12 12:42:17 -0500

    Resopaldo

--- public_html/wp-content/plugins/hostinger/includes/class-hostinger-deactivator.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined( 'ABSPATH' ) || exit;
-
-class Hostinger_Deactivator {
-	public static function deactivate(): void {
-		wp_clear_scheduled_hook( 'weekly_admin_surveys_event' );
-	}
-}
